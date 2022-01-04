e2c069767263a70355a131c29b3a2dac641fa55a
Dallon Asnes <dallon.asnes@gmail.com>
2022-01-03 21:57:56 -0600

    Default debug to false

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -10,7 +10,7 @@ export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
 export const ALCHEMY_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 // Used for debugging logged in flow locally without spending ETH
-export const DEBUG_TRANSACTIONS = true;
+export const DEBUG_TRANSACTIONS = false;
 
 export const NETWORKS = {
   localhost: {
